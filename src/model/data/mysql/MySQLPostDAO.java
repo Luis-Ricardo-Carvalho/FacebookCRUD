10b9f72d95803427978e236736ba2ae71b684820
Luis-Ricardo-Carvalho <luisricardocarvalholin@gmail.com>
2025-09-24 09:17:13 -0300

    Primeira vesão

---
+++ src/model/data/mysql/MySQLPostDAO.java
@@ -0,0 +1,180 @@
+package model.data.mysql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import model.ModelException;
+import model.Post;
+import model.User;
+import model.data.PostDAO;
+import model.data.mysql.utils.DAOUtils;
+import model.data.mysql.utils.MySQLConnectionFactory;
+
+public class MySQLPostDAO implements PostDAO {
+
+	@Override
+	public void save(Post post) throws ModelException {	
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+
+			String sqlIsert = " INSERT INTO posts " + 
+			                  " VALUES (DEFAULT, ?, CURDATE(), ?); ";
+
+			preparedStatement = connection.prepareStatement(sqlIsert);
+			preparedStatement.setString(1, post.getContent());
+			preparedStatement.setInt(2, post.getUser().getId());
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao inserir post do BD.", sqle);
+		} catch (ModelException me) {
+			throw me;
+		} 
+		finally {
+			DAOUtils.close(preparedStatement);
+			DAOUtils.close(connection);
+		}
+	}
+
+	@Override
+	public void update(Post post) throws ModelException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+			
+			String sqlUpdate = " UPDATE posts "
+					+ " SET "
+					+ " content = ?, "
+					+ " post_date = CURDATE() "
+					+ " WHERE id = ?; ";
+			
+			preparedStatement = connection.prepareStatement(sqlUpdate);
+			preparedStatement.setString(1, post.getContent());
+			preparedStatement.setInt(2, post.getId());
+			
+			preparedStatement.executeUpdate();
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao atualizar post do BD.", sqle);
+		} catch (ModelException me) {
+			throw me;
+		} finally {
+			DAOUtils.close(preparedStatement);
+			DAOUtils.close(connection);
+		}
+	}
+
+	@Override
+	public void delete(Post post) throws ModelException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();  
+
+			String sqlDelete = "delete from posts where id = ?;";
+
+			preparedStatement = connection.prepareStatement(sqlDelete);
+			preparedStatement.setInt(1, post.getId());
+
+			preparedStatement.executeUpdate();
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao excluir post do BD.", sqle);
+		} finally {
+			DAOUtils.close(preparedStatement);
+			DAOUtils.close(connection);
+		}
+	}
+
+	@Override
+	public List<Post> findAll() throws ModelException {
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet rs = null;
+		List<Post> postsList = new ArrayList<>();
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+
+			statement = connection.createStatement();
+			String sqlSelect = " SELECT * FROM posts; ";
+
+			rs = statement.executeQuery(sqlSelect);
+
+			setUpUsers(rs, postsList);
+
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao carregar posts do BD.", sqle);
+		} finally {
+			DAOUtils.close(rs);
+			DAOUtils.close(statement);
+			DAOUtils.close(connection);
+		}
+
+		return postsList;
+	}
+
+	@Override
+	public List<Post> findByUserId(int userId) throws ModelException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet rs = null;
+		List<Post> postsList = new ArrayList<>();
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+
+			String sqlSelect = " SELECT * FROM posts WHERE user_id = ?; ";
+
+			preparedStatement = connection.prepareStatement(sqlSelect);
+			preparedStatement.setInt(1, userId);
+			
+			rs = preparedStatement.executeQuery();
+			
+			setUpUsers(rs, postsList);
+
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao carregar posts do BD.", sqle);
+		} finally {
+			DAOUtils.close(rs);
+			DAOUtils.close(preparedStatement);
+			DAOUtils.close(connection);
+		}
+
+		return postsList;
+	}
+
+	private void setUpUsers(ResultSet rs, List<Post> postsList) throws SQLException {
+		
+		while (rs.next()) {
+			int postId = rs.getInt("id"); 
+			String postContent = rs.getString("content");
+			Date postDate = rs.getDate("post_date");
+			int userId = rs.getInt("user_id");
+
+			Post newPost = new Post(postId);
+			newPost.setContent(postContent);
+			newPost.setDate(postDate);
+			
+			User postUser = new User(userId);
+			// TODO buscar nomes do usuario
+			postUser.setName("Colocar nome");
+			newPost.setUser(postUser);
+			
+			postsList.add(newPost);
+		}
+		
+	}
+	
+}
---
+++ src/model/data/mysql/MySQLUserDAO.java
@@ -0,0 +1,151 @@
+package model.data.mysql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import model.ModelException;
+import model.Post;
+import model.User;
+import model.data.UserDAO;
+import model.data.mysql.utils.DAOUtils;
+import model.data.mysql.utils.MySQLConnectionFactory;
+
+public class MySQLUserDAO implements UserDAO{
+
+	@Override
+	public void save(User user) throws ModelException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+
+			String sqlIsert = " INSERT INTO "
+					+ " users VALUES "
+					+ " (DEFAULT, ?, ?, ?); ";
+
+			preparedStatement = connection.prepareStatement(sqlIsert);
+			preparedStatement.setString(1, user.getName());
+			preparedStatement.setString(2, user.getGender().toString());
+			preparedStatement.setString(3, user.getEmail());
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao inserir user do BD.", sqle);
+		} catch (ModelException me) {
+			throw me;
+		} 
+		finally {
+			DAOUtils.close(preparedStatement);
+			DAOUtils.close(connection);
+		}		
+	}
+
+	@Override
+	public void update(User user) throws ModelException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+
+			String sqlUpdate = " UPDATE users SET "
+					+ " nome = ?, sexo = ?, "
+					+ " email = ? where id = ?; ";
+
+			preparedStatement = connection.prepareStatement(sqlUpdate);
+			preparedStatement.setString(1, user.getName());
+			preparedStatement.setString(2, user.getGender().toString());
+			preparedStatement.setString(3, user.getEmail());
+			preparedStatement.setInt(4, user.getId());
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao atualizar user do BD.", sqle);
+		} catch (ModelException me) {
+			throw me;
+		} 
+		finally {
+			DAOUtils.close(preparedStatement);
+			DAOUtils.close(connection);
+		}		
+	}
+
+	@Override
+	public void delete(User user) throws ModelException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+
+			String sqlDelete = " DELETE FROM users WHERE id = ?; ";
+
+			preparedStatement = connection.prepareStatement(sqlDelete);
+			preparedStatement.setInt(1, user.getId());
+
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao deletar user do BD.", sqle);
+		} catch (ModelException me) {
+			throw me;
+		} 
+		finally {
+			DAOUtils.close(preparedStatement);
+			DAOUtils.close(connection);
+		}		
+	}
+
+	@Override
+	public List<User> findAll() throws ModelException {
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet rs = null;
+		List<User> usersList = new ArrayList<>();
+
+		try {
+			connection = MySQLConnectionFactory.getConnection();
+
+			statement = connection.createStatement();
+			String sqlSelect = " SELECT * FROM users; ";
+
+			rs = statement.executeQuery(sqlSelect);
+
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("nome");
+				String gender = rs.getString("sexo");
+				String email = rs.getString("email");
+				
+				User user = new User(id);
+				user.setName(name);
+				user.setGender(gender.charAt(0));
+				user.setEmail(email);
+				
+				List<Post> posts = new MySQLPostDAO().findByUserId(id);
+				user.setPosts(posts);
+				
+				usersList.add(user);
+			}
+
+		} catch (SQLException sqle) {
+			DAOUtils.sqlExceptionTreatement("Erro ao carregar posts do BD.", sqle);
+		} finally {
+			DAOUtils.close(rs);
+			DAOUtils.close(statement);
+			DAOUtils.close(connection);
+		}
+
+		return usersList;
+	}
+
+}
